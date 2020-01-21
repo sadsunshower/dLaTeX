d8532429ebfaf89fba70ed4dd19ce5fcbfb18b7f
Nicholas Berridge-Argent <nicholas.berridgeargent@gmail.com>
2020-01-21 15:25:58 +1100

    Changed the way errors with no line are handled

--- message_generators/generator.py
+++ message_generators/generator.py
@@ -26,6 +26,13 @@ class GeneratorLookup(Generator):
         with open(os.path.join(sys.path[0], 'errors.json'), 'r') as f:
             self._errors = json.loads(f.read())
     
+    def _error_lookup(self, msg: str) -> str:
+        for error in self._errors:
+            if msg.startswith(error):
+                return self._errors[error]
+        
+        return f'Unknown error: {msg}'
+    
     def trigger_output(self, output: typing.List[str], file: typing.List[str]) -> typing.List[messages.Message]:
         ret = []
 
@@ -35,14 +42,25 @@ class GeneratorLookup(Generator):
                 while not output[curr].startswith('l.'):
                     curr += 1
                 
-                num = int(re.search(r'^l.([0-9]+) ', output[curr]).group(1))
-                pos = len(re.sub(r'^l.[0-9]+ ', r'', output[curr]))
-                msg = re.sub(r'^! ', r'', line)
+                    if curr == len(output):
+                        curr = -1
+                        break
+                
+                if curr != -1:
+                    num = int(re.search(r'^l.([0-9]+) ', output[curr]).group(1))
+                    pos = len(re.sub(r'^l.[0-9]+ ', r'', output[curr]))
+                    msg = re.sub(r'^! ', r'', line)
 
-                ret.append(messages.Message(
-                    messages.PositionInfo((pos - 1, ), file[num - 1], num),
-                    self._errors.get(msg, f'Unknown error: {msg}'),
-                    messages.Severity.ERROR
-                ))
+                    ret.append(messages.Message(
+                        messages.PositionInfo((pos - 1, ), file[num - 1], num),
+                        self._error_lookup(msg),
+                        messages.Severity.ERROR
+                    ))
+                else:
+                    ret.append(messages.Message(
+                        None,
+                        self._error_lookup(re.sub(r'^! ', r'', line)),
+                        messages.Severity.ERROR
+                    ))
 
         return ret
